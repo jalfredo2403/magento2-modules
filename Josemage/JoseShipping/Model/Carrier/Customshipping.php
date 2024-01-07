5bfcb5a4b2bd573385d3e5f1e8081d09af6cb1db
Jose hernandez <jahernadezp@gmail.com>
2024-01-07 03:37:58 -0500

    Add shipping module

---
+++ Josemage/JoseShipping/Model/Carrier/Customshipping.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types=1);
+
+namespace Josemage\JoseShipping\Model\Carrier;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\Method;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Psr\Log\LoggerInterface;
+
+/**
+ *
+ */
+class Customshipping extends AbstractCarrier implements CarrierInterface
+{
+    /**
+     *
+     */
+    const FLATRATE = 5;
+    /**
+     * @var string
+     */
+    protected  $_code = 'joseshipping';
+
+    /**
+     * @var bool
+     */
+    protected  $_isFixed = true;
+
+    /**
+     * @var ResultFactory
+     */
+    private ResultFactory $rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    private MethodFactory $rateMethodFactory;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ErrorFactory $rateErrorFactory
+     * @param LoggerInterface $logger
+     * @param ResultFactory $rateResultFactory
+     * @param MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory         $rateErrorFactory,
+        LoggerInterface      $logger,
+        ResultFactory        $rateResultFactory,
+        MethodFactory        $rateMethodFactory,
+        array                $data = []
+    )
+    {
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+    }
+
+    /**
+     * Custom Shipping Rates Collector
+     *
+     * @param RateRequest $request
+     * @return \Magento\Shipping\Model\Rate\Result|bool
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        /** @var Method $method */
+        $method = $this->rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $shippingCost = (float)$this->getConfigData('shipping_cost');
+        $shippingCost += static::FLATRATE;
+        $method->setPrice($shippingCost);
+        $method->setCost($shippingCost);
+
+        /** @var Result $result */
+        $result = $this->rateResultFactory->create();
+        $result->append($method);
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllowedMethods(): array
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
