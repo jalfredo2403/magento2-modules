1cefbb8c95f0295df859e534512bf398a48edb1e
Jose hernandez <jahernadezp@gmail.com>
2024-02-28 00:22:19 -0500

    Create a secdn compnent and working togeter

--- Josemage/Component/view/frontend/web/js/view/ob-component.js
+++ Josemage/Component/view/frontend/web/js/view/ob-component.js
@@ -1,24 +1,39 @@
 define([
     'uiComponent',
-    'ko'
-], function (Component, ko) {
+    'ko',
+    'Josemage_Component/js/model/counter'
+], function (Component, ko, Counter) {
     "use strict";
 
     return Component.extend({
         defaults: {
             title: ko.observable('My heading'),
             content: ko.observable('<strong>Jose</strong>'),
-            counter: ko.observable(0)
+            counter: Counter,
         },
 
         initialize: function () {
             this._super();
             this.title("Jose A Hernandez").content('<strong>Sample of observables in Magento 2</strong> By Jose');
             this.content.subscribe(function () {
-                this.counter(
-                    document.getElementById('custom_input').value.length
-                );
+                const str = document.getElementById('custom_input').value;
+                const digitCount = str.match(/\d/g)?.length || 0;
+                Counter(digitCount);
             }, this);
+            let self = this;
+
+            setTimeout(function () {
+                self.title("Jose A Hernandez");
+            }, 3000);
+
+            this.title.subscribe(function (newValue) {
+                const newTitle =  "Heading value update: " + newValue;
+                alert(newTitle);
+            });
+
+            this.title.extend({
+                notify: 'always',
+            });
         },
         onChangeInput: function () {
             let inputVal = document.getElementById('custom_input').value;
